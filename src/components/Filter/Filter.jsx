de2400744aee6b9e9c5bfbbdcd0db8e2dee4eadb
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-08-03 20:33:21 +0300

    delete code

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -25,34 +25,8 @@ export const Filter = () => {
   );
 };
 
-
-
 Filter.propTypes = {
   changeFilterInput: PropTypes.func,
 };
 
-// =============================================
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setFilter, getFilter } from '../../redux/slice';
-// import './Filter.module.css';
 
-// export const Filter = () => {
-//   const dispatch = useDispatch();
-//   const filter = useSelector(getFilter);
-
-//   const onChangeFilter = (e) => {
-//     dispatch(setFilter(e.target.value));
-//   };
-
-//   return (
-//     <label>
-//       <input
-//         type="text"
-//         name="filter"
-//         value={filter}
-//         onChange={onChangeFilter}
-//         placeholder="Find contacts by name"
-//       />
-//     </label>
-//   );
-// };
